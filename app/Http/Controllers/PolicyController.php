415bb0dec05089160d9f325882dfdba468ff60b8
rashidul191 <rashidul.191cse.gub@gmail.com>
2025-04-13 22:14:57 +0600

    create Helper funciton folder and file also write code of controller files

---
+++ app/Http/Controllers/PolicyController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Policy;
+use Illuminate\Http\Request;
+
+class PolicyController extends Controller
+{
+    //
+
+    function PolicyByType(Request $request){
+        return Policy::where('type', '=', $request->type)->first();
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\ResponseHelper;
+use App\Models\Product;
+use App\Models\ProductDetail;
+use App\Models\ProductReview;
+use App\Models\ProductSlider;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+
+    public function ListProductByCategory(Request $request)
+    {
+        $data = Product::where('category_id', $request->id)->with('brand', 'category')->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function ListProductByRemark(Request $request)
+    {
+        $data = Product::where('remark', $request->remark)->with('brand', 'category')->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function ListProductByBrand(Request $request)
+    {
+        $data = Product::where('brand_id', $request->id)->with('brand', 'category')->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function ListProductSlider(Request $request)
+    {
+        $data = ProductSlider::all();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function ProductDetailsById(Request $request)
+    {
+        $data = ProductDetail::where('product_id', '=', $request->id)->with('product', 'product.brand', 'product.category')->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function ListReviewByProduct(Request $request)
+    {
+        $data = ProductReview::where('product_id', '=', $request->id)->with(
+            ['customerProfile' => function ($query) {
+                $query->select('id', 'cus_name');
+            }]
+        )->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+}
