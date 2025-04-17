ed943a8f68c6495dbf14a0ae608524be5603242f
rashidul191 <rashidul.191cse.gub@gmail.com>
2025-04-17 09:34:03 +0600

    create muster layout and home-page blade file, also connection asset to layout app.blade file

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\PolicyController;
 use App\Http\Controllers\ProductCartController;
@@ -23,9 +24,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Home Page
+Route::get('/', [HomeController::class, 'HomePage']);
+Route::get('/home', [HomeController::class, 'HomePage']);
+
 
 // Brand List
 Route::get('/BrandList', [BrandController::class, 'BrandList']);
